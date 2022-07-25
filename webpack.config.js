468f2d8e5fe253671a3ef9d04969cf8aac0d1e12
marfyl <marfyl.dev@gmail.com>
2022-07-25 14:09:43 +0200

    remove doublequote for webpack files

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
-const path = require("path");
-const pkg = require("./package.json");
-const camelcase = require("camelcase");
-const process = require("process");
+const path = require('path');
+const pkg = require('./package.json');
+const camelcase = require('camelcase');
+const process = require('process');
 const env = process.env;
 
 const NODE_ENV = env.NODE_ENV;
-const PROD = NODE_ENV === "production";
-const SRC_DIR = "./src";
+const PROD = NODE_ENV === 'production';
+const SRC_DIR = './src';
 
 let config = {
   // unless we are in production, use inline-source-map development tool
   // which helps track down bugs
-  devtool: PROD ? false : "inline-source-map",
+  devtool: PROD ? false : 'inline-source-map',
 
   // entry point - src/index.js
-  entry: path.join(__dirname, SRC_DIR, "cytoscape-node-editing.js"),
+  entry: path.join(__dirname, SRC_DIR, 'cytoscape-node-editing.js'),
 
   // webpack throws warning if not provided a default mode
   // use the 'build:dev' script if you want development mode with non-minified file
   // this mode is used in 'build' script
-  mode: "production",
+  mode: 'production',
   output: {
     path: path.join(__dirname),
-    filename: pkg.name + ".js",
+    filename: pkg.name + '.js',
     library: camelcase(pkg.name),
-    libraryTarget: "umd",
+    libraryTarget: 'umd',
   },
   // loader
   module: {
@@ -32,7 +32,7 @@ let config = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: 'babel-loader',
       },
     ],
   },
